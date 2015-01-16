88d15265a14555dc3fda39f7e6dfad3fe8ab4edc
negamann303 <negamann303@gmail.com>
2015-01-16 00:20:58 +0000

    Initial Kernel Files (Part 1)

---
+++ compiled/zip.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export INITRAMFS=`readlink -f .`
+export INSTALLER=$INITRAMFS/installer
+REVISION="$(svn info https://kernel-ng2-negalite.googlecode.com/svn/ | grep "^Revision:" | cut -c 11-)"
+
+perl repack-bootimg.pl zImage boot.img-ramdisk boot.img 
+mv boot.img ./installer/kernel/boot.img
+
+echo "Zipping The Kernel Up For Flashable Package"
+cd $INSTALLER
+
+zip -9 -r negalite_kernel_NG2 cpu kernel META-INF setup system
+mv $INSTALLER/negalite_kernel_NG2.zip $INITRAMFS/negalite_kernel_NG2_r$REVISION_zipped.zip
+echo "Done"
