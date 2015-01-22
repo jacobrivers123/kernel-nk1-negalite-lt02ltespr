bd5f2595aacced2a09d4e3d866b8cb8181f7c8eb
negamann303@gmail.com <negamann303@gmail.com@400b3b80-593e-74ec-52af-076da25c708d>
2015-01-22 03:00:25 +0000

    OverClocking: Achieved(35 Steps Total/Max: 1.78GHz) Tweaked/Added FSB Frequencies Zero'd PVS Table(@1700MHz) Removed Unneeded Debugging Optimized Scheduler Functionality

--- include/linux/acpuclock-defines.h
+++ include/linux/acpuclock-defines.h
@@ -1,6 +1,6 @@
 #define FREQ_TABLE_SIZE		35
 #define FREQ_TABLE_SIZE_OFFSET	8
-#define FREQ_STEPS		35
+#define FREQ_STEPS		38
 #define MAX_VDD_SC		1450000 /* uV */
 #define MIN_VDD_SC		800000 /* uV */
 #define MAX_VDD_MEM_DIG		1150000 /* uV */
